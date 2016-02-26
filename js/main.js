8c8cdd9c276617b36cfe808e122a636f4e3c4807
kaylaboch <boch.kayla57@gmail.com>
2016-02-26 18:13:09 -0500

    cmiiting

--- js/main.js
+++ js/main.js
@@ -21,13 +21,13 @@ var aPast = prompt('A past tense verb (ran, burped, etc.)');
 var aStory = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
 
 if (aStory == 'a') {
-    document.write('After snacking on + ' ' + anAdjective + treats, the + ' ' + aColour + bellied + ' ' + aCreature + ' ' + aPast + for hours.');
+    document.write('After snacking on '  + anAdjective + ' treats, the ' + aColour + ' bellied ' + aCreature + ' ' + aPast + ' for hours.');
 }
 
 if (aStory == 'b') {
-    document.write('Ameilia + ' ' + aPast + through the + ' ' + anAdjective + ' ' + aColour + nebula escaping the space + ' ' + aCreature.');
+    document.write('Ameilia ' + aPast + ' through the ' + anAdjective + ' ' + aColour + ' nebula escaping the space ' + aCreature.'');
 }
 
 if (aStory == 'c') {
-    document.write('Jackson chose his + ' ' + anAdjective + ,+ ' ' + aColour + ' ' + aCreature + card and + ' ' + aPast + it to the table knowing he won.');
+    document.write('Jackson chose his ' + anAdjective + ',' + aColour + ' ' + aCreature + ' card and ' + aPast + ' it to the table knowing he won.');
 }
