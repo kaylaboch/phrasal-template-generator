eed56dc4c1d9af1dc7230a57c65dc02e79db79ba
kaylaboch <boch.kayla57@gmail.com>
2016-02-26 18:21:43 -0500

    alert

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-alert('Lets make a story together!')
-
 var aColour;
 
 var aCreature;
@@ -20,14 +18,16 @@ var aPast = prompt('A past tense verb (ran, burped, etc.)');
 
 var aStory = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
 
+alert("Let's make a story together!");
+
 if (aStory == 'a') {
-    document.write('After snacking on '  + anAdjective + ' treats, the ' + aColour + ' bellied ' + aCreature + ' ' + aPast + ' for hours.');
+  document.write('After snacking on '  + anAdjective + ' treats, the ' + aColour + ' bellied ' + aCreature + ' ' + aPast + ' for hours.');
 }
 
 if (aStory == 'b') {
-    document.write('Ameilia ' + aPast + ' through the ' + anAdjective + ' ' + aColour + ' nebula escaping the space ' + aCreature '.');
+  document.write('Ameilia ' + aPast + ' through the ' + anAdjective + ' ' + aColour + ' nebula escaping the space ' + aCreature + '.');
 }
 
 if (aStory == 'c') {
-    document.write('Jackson chose his ' + anAdjective + ',' + aColour + ' ' + aCreature + ' card and ' + aPast + ' it to the table knowing he won.');
+  document.write('Jackson chose his ' + anAdjective + ',' + aColour + ' ' + aCreature + ' card and ' + aPast + ' it to the table knowing he won.');
 }
