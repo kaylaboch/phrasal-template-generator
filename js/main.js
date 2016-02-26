9fc75e1df2d74aa1ff26ce49fb579156585627e0
kaylaboch <boch.kayla57@gmail.com>
2016-02-26 18:04:11 -0500

    commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+alert('Lets make a story together!')
+
+var aColour;
+
+var aCreature;
+
+var anAdjective;
+
+var aPast;
+
+var aStory;
+
+var aColour = prompt('A colour (orange, indigo, etc.)');
+
+var aCreature = prompt('A creature (dragon, moth, etc.)');
+
+var anAdjective = prompt('An adjective (beautiful, super, etc.)');
+
+var aPast = prompt('A past tense verb (ran, burped, etc.)');
+
+var aStory = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
+
+if (aStory == 'a') {
+    document.write('After snacking on + ' ' + anAdjective + treats, the + ' ' + aColour + bellied + ' ' + aCreature + ' ' + aPast + for hours.');
+}
+
+if (aStory == 'b') {
+    document.write('Ameilia + ' ' + aPast + through the + ' ' + anAdjective + ' ' + aColour + nebula escaping the space + ' ' + aCreature.');
+}
+
+if (aStory == 'c') {
+    document.write('Jackson chose his + ' ' + anAdjective + ,+ ' ' + aColour + ' ' + aCreature + card and + ' ' + aPast + it to the table knowing he won.');
+}
