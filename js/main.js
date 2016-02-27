ff4f6f6d421ae0bbbe40168502d3b119ad8eab41
kaylaboch <boch.kayla57@gmail.com>
2016-02-26 21:39:51 -0500

    moved alert

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ var aPast;
 
 var aStory;
 
+alert("Let's make a story together!");
+
 var aColour = prompt('A colour (orange, indigo, etc.)');
 
 var aCreature = prompt('A creature (dragon, moth, etc.)');
@@ -17,8 +19,6 @@ var anAdjective = prompt('An adjective (beautiful, super, etc.)');
 var aPast = prompt('A past tense verb (ran, burped, etc.)');
 
 var aStory = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
-
-alert("Let's make a story together!");
 
 if (aStory == 'a') {
   document.write('After snacking on '  + anAdjective + ' treats, the ' + aColour + ' bellied ' + aCreature + ' ' + aPast + ' for hours.');
